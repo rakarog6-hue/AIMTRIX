b4922ac4ed8ebf0d9f69f27c2770075e0c0d3fa2
rakarog6-hue <rakarog6@gmail.com>
2026-02-28 08:31:49 +0700

    Implement game boost feature in MainActivity
    Added button click listener to boost gaming performance.

--- java/com/aimtrix/optimizer/MainActivity.java
+++ java/com/aimtrix/optimizer/MainActivity.java
@@ -2,6 +2,9 @@ package com.aimtrix.optimizer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +12,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Button boost = findViewById(R.id.btnOptimize);
+
+        boost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                GamingOptimizer optimizer = new GamingOptimizer();
+                optimizer.optimize();
+
+                Toast.makeText(MainActivity.this, "Game Boosted 🚀", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
 }
